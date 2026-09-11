9ab9cca4f03e0a4b4218f62e5f2d3f0555bff01e
Ishaku Lawan <lawanishaku14@gmail.com>
2026-09-11 16:47:37 +0100

    feat: implement task management API with CRUD operations

---
+++ server/controllers/task.controller.js
@@ -0,0 +1,57 @@
+const taskServices = require("../services/task.service");
+
+exports.getAllTasks = async (req, res) => {
+  try {
+    const category = req.query.category;
+    const tasks = await taskServices.getAllTasks(category);
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(err.status || 500).json({ error: err.message || "Internal server error: can't get tasks" });
+  }
+}
+
+exports.createTask = async (req, res) => {
+  try { 
+    const task = await taskServices.createTask(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(err.status || 500).json({ error: err.message || "Internal server error: can't create task" });
+  }
+}
+
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await taskServices.getTaskById(req.params.id);
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(err.status || 500).json({ error: err.message || "Error retrieving task" });
+  }
+}
+
+exports.updateTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await taskServices.updateTask(taskId, req.body);
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(err.status || 500).json({ error: err.message || "Error updating task status" });
+  }
+}
+
+exports.updateTaskStatus = async (req, res) => {
+  try {
+    const status = await taskServices.updateTaskStatus(req.params.id, req.body.status);
+    res.status(200).json({status});
+  } catch (err) {
+    res.status(err.status || 500).json({ error: err.message || "Error updating task status" });
+  }
+}
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const deletedTask = await taskServices.deleteTask(req.params.id);
+    res.status(200).json(deletedTask)
+  } catch (err) {
+    res.status(err.status || 500).json({ error: err.message || "Error deleting task" });
+  }
+}
